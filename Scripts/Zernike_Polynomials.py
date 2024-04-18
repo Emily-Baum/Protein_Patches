081f87a93bb205f0cceee9b124cc1496c6a0cc5d
Emily-Baum <123198631+Emily-Baum@users.noreply.github.com>
2024-04-18 00:41:59 -0400

    Added feature descriptor functionality

--- Scripts/Zernike_Polynomials.py
+++ Scripts/Zernike_Polynomials.py
@@ -13,28 +13,32 @@ def zernike_polynomials(voxels,N=20):
 
     Parameters
     ----------
-    X : numpy array of float
+    voxels : numpy array of float
     
         The coordinates of the points at which to calculate the value of the 
-        3D Zernike function.
+        3D Zernike function. A voxelized grid of evenly spaced points is recommended
+        so that the polynomials are defined across the entire unit sphere.
         
-    n : int, optional
+    N : int, optional
     
         Order of approximation of the 3D Zernike function. Higher order provides
-        higher resolution but scales computation exponentially. The default is 20.
+        higher resolution but increases computational time. Choosing an order too
+        large may lead to overfitting. Must be consistent across polynomials,
+        moments, and descriptors. The default is 20.
 
     Returns
     -------
-    Z : list of float
+    Z : numpy array of float
     
-        Calculates the value of the 3D Zernike function at a given coordinate within a unit sphere.
-        Equations numbers follow: https://doi.org/10.1002/prot.22030.
+        Calculates the value of the 3D Zernike function at a given set of coordinates 
+        within a unit sphere. Equations numbers follow: https://doi.org/10.1002/prot.22030.
 
     """
     
     import math
     import numpy as np
-
+    
+    # radius of point from patch center
     r = (voxels[:,0]**2 + voxels[:,1]**2 + voxels[:,2]**2)**.5
 
     Z = []
@@ -75,27 +79,32 @@ def zernike_polynomials(voxels,N=20):
                     # eqn 7
                     Znlm = elm*qsum
                     Z.append(Znlm)
+                    
     Z = np.asarray(Z).T
     return Z
         
 #%%
 
-def voxelize(scaled_patch,gridpoints=20):
+def voxelize(scaled_patch,gridpoints=20,feature_array=None):
+    
     """
     
     Parameters
     ----------
     scaled_patch : numpy array of float
     
-        The dictionary contains the scaled coordinates of all surface points
-        (and opt atom centers) within patch_radius of a random surface point.
-        Dictionary entries are named by the centerpoint_index of each patch.
+        The scaled coordinates of all surface points (and opt atom centers) within 
+        patch_radius of a random surface point.
         
     gridpoints : int, optional
     
         The number of voxels in each dimension to construct a unit grid (gridpoints**3).
         Only the voxels within a unit sphere are retained. The default is 20.
 
+    feature_array: numpy array of float, optional
+    
+        An array of features to map to the voxelized grid. The default is None.
+        
     Returns
     -------
     voxels : list of float
@@ -106,33 +115,43 @@ def voxelize(scaled_patch,gridpoints=20):
     
         The mapping of the surface patch to the voxel grid. The values in the 
         list are 1 if the voxel contains part of the surface, or 0 if it does not.
+        
+    voxel_feat: numpy array of float, optional
+    
+        The values of the features defined in feature_array for each voxel. Empty voxels
+        are given a default value of 0, and voxels containing multiple surface points
+        are assigned an average value.
 
     """
     
     import numpy as np
     
+    # create a unit grid with gridpoints**3 points, saving only those inside unit sphere
     x = np.linspace(-1,1,gridpoints)
     voxels = [[i,j,k] for i in x for j in x for k in x if i**2 + j**2 + k**2 <= 1]
     
+    # map the surface patch to the voxelized coordinate grid
     voxel_surf = voxel_surface(scaled_patch,voxels,gridpoints)
     
-    #TODO
-    #voxel_feat = voxel_features(scaled_patch,features,voxels,gridpoints)
-    
-    return voxels, voxel_surf
+    # if a feature array is specified, map the features to the voxels
+    if feature_array is None:
+        return voxels, voxel_surf
+    else:
+        voxel_feat = voxel_features(scaled_patch,feature_array,voxels,gridpoints)
+        return voxels, voxel_surf, voxel_feat
 
 #%%
 
 def voxel_surface(scaled_patch,voxels,gridpoints):
+    
     """
     
     Parameters
     ----------
-    scaled_patch : dict of numpy arrays
+    scaled_patch : numpy array of float
     
-        The dictionary contains the scaled coordinates of all surface points
-        (and opt atom centers) within patch_radius of a random surface point.
-        Dictionary entries are named by the centerpoint_index of each patch.
+        The scaled coordinates of all surface points (and opt atom centers) within 
+        patch_radius of a random surface point.
         
     voxels : list of float
     
@@ -152,54 +171,209 @@ def voxel_surface(scaled_patch,voxels,gridpoints):
 
     """
 
-    import numpy as np    
-
+    import numpy as np
+    
+    # calculate the spacing between each voxel
+    # half the distance between each voxel center
     voxel_spacing = 1/(gridpoints-1)
     
+    # check if any patch points are within each cubic voxel
     voxel_surf = np.zeros(len(voxels))
     for v_coords in voxels:
         for p_coords in scaled_patch:
             if abs(v_coords[0] - p_coords[0]) < voxel_spacing:
                 if abs(v_coords[1] - p_coords[1]) < voxel_spacing:
                     if abs(v_coords[2] - p_coords[2]) < voxel_spacing:
+                        
+                        # if a surface point falls within the voxel, mark the voxel as full
                         voxel_surf[voxels.index(v_coords)] = 1
                         continue
+                    
     return voxel_surf
 
-#%% 
-# TODO work in progress
-def zernike_descriptor(components,voxel_surf,gridpoints=20,N=20):
+#%%
+def voxel_features(scaled_patch,feature_array,voxels,gridpoints):
     
+    """
+    
+    Parameters
+    ----------
+    scaled_patch : numpy array of float
+    
+        The scaled coordinates of all surface points (and opt atom centers) within 
+        patch_radius of a random surface point.
+        
+    feature_array: numpy array of float
+    
+        An array of features to map to the voxelized grid.
+        
+    voxels : list of float
+    
+        The coordinates of each voxel certerpoint.
+        
+    gridpoints : int
+    
+        The number of voxels in each dimension to construct a unit grid (gridpoints**3).
+        Only the voxels within a unit sphere are retained.
+
+    Returns
+    -------
+    voxel_feat : numpy array of float
+    
+        The mapping of the surface patch features to the voxel grid. Empty voxels
+        are assigned a default value of 0. Voxels containing multiple surface points
+        are assigned the average feature value within the voxel.
+
+    """
+
     import numpy as np
     
-    zernike_fit = np.linalg.lstsq(components,voxel_surf,rcond=None)[0]
+    # calculate the spacing between each voxel
+    # half the distance between each voxel center
+    voxel_spacing = 1/(gridpoints-1)
+    
+    # check if any patch points are within each cubic voxel
+    voxel_feat = np.zeros((len(voxels),np.shape(feature_array)[1]))
+    for v_coords in voxels:
+        
+        # count number of surface points in voxel
+        n_in_vox = 0
+        for p_coords in range(len(scaled_patch)):
+            if abs(v_coords[0] - scaled_patch[p_coords][0]) < voxel_spacing:
+                if abs(v_coords[1] - scaled_patch[p_coords][1]) < voxel_spacing:
+                    if abs(v_coords[2] - scaled_patch[p_coords][2]) < voxel_spacing:
+                        
+                        # if a surface point falls within the voxel, add feature value
+                        voxel_feat[voxels.index(v_coords)] += feature_array[p_coords]
+                        n_in_vox += 1
+        
+        if n_in_vox == 0:
+            # leave default empty voxel value as 0
+            continue
+        else:
+            # take the average value in the voxel
+            voxel_feat[voxels.index(v_coords)] /= n_in_vox
+            
+    return voxel_feat
+
+#%% 
+def zernike_moments(z_polynomials,voxel_surf,gridpoints=20,N=20):
+    
+    """
+    
+    Parameters
+    ----------
+    z_polynomials : numpy array of float
+    
+        The value of the 3D Zernike function at a given set of coordinates
+        within a unit sphere.
+        
+    voxel_surf : numpy array of float
+    
+        The mapping of the surface patch to the voxel grid. The values in the 
+        list are 1 if the voxel contains part of the surface, or 0 if it does not.
+        Alternatively, an array of values for mapped features can be used as input.
+        
+    gridpoints : int, optional
+    
+        The number of voxels in each dimension to construct a unit grid (gridpoints**3).
+        Only the voxels within a unit sphere are retained. The default is 20.
+        
+    N : int, optional
+    
+        Order of approximation of the 3D Zernike function. Higher order provides
+        higher resolution but increases computational time. Choosing an order too
+        large may lead to overfitting. Must be consistent across polynomials,
+        moments, and descriptors. The default is 20.
+
+    Returns
+    -------
+    corrected_moments : numpy array of float
+    
+        Calculates the 3D Zernike moments, necessary for computation of the Zernike 
+        descriptors. Equation numbers follow: https://doi.org/10.1145/781606.781639
+
+    """
+        
+    import numpy as np
+    
+    zernike_fit = np.linalg.lstsq(z_polynomials,voxel_surf,rcond=None)[0]
     
     voxel_spacing = 1/(gridpoints-1)
     
-    zernike_moments = 3/(4*np.pi)*zernike_fit*voxel_spacing**3
-    sq_moments = zernike_moments**2
+    # eqn. 8.2 (omega as a function of f)
+    moments = 3/(4*np.pi)*zernike_fit*(voxel_spacing)**3
+    
+    # following the symmetry relation in eqn. 9
+    corrected_moments = np.real(moments)
+    omega_index = 0
+    
+    # index which moments come from -m terms
+    for n in range(0,N+1):
+        for l in range(n+1): # l goes from 0 to n
+            if ((n-l)%2) == 0: # (n-l) even
+                for m in range(-l,l+1): # m goes from -l to l
+                    if m < 0:
+                        pos_m = int(omega_index + -2*m)
+                        
+                        # eqn. 9
+                        corrected_moments[omega_index] = corrected_moments[pos_m]*(-1)**m
+                    omega_index += 1
+                    
+    return corrected_moments
+    
+#%%
+def zernike_descriptor(moments,N=20):
+    
+    """
+
+    Parameters
+    ----------
+    moments : numpy array of float
+    
+        The 3D Zernike moments for a patch voxelized within a unit sphere.
+        The moments can contain geometric or feature information.
+        
+    N : int, optional
+    
+        Order of approximation of the 3D Zernike function. Higher order provides
+        higher resolution but increases computational time. Choosing an order too
+        large may lead to overfitting. Must be consistent across polynomials,
+        moments, and descriptors. The default is 20.
+
+    Returns
+    -------
+    zernike_descriptor : numpy array of float
+    
+        A compact, non-redundant, rotationally invariant descriptor used to 
+        represent protein surface patches.
+        Equations numbers follow: https://doi.org/10.1002/prot.22030.
+
+    """
+    
+    import numpy as np
+    sq_moments = moments**2
     
     # determine the total number of descriptors based on the order N
     n = np.asarray(range(1,N+2))
     F_nl = ( n - ( (-1) + (-1)**n )/2 )/2
-    zernike_descriptor = np.zeros(sum(F_nl))
+    
+    # creating an array of length F_nl, following eqn. 10
+    moment_sum = np.zeros(int(sum(F_nl)))
     
     # sum the squared moments for each zernike descriptor 
-    omega_nlm = 0
+    omega_index = 0
+    zd_index = 0
     for n in range(0,N+1):
         for l in range(n+1): # l goes from 0 to n
-            for m in range(-l,l+1): # m goes from -l to l
-                zernike_descriptor[F_nl] += sq_moments["indexing"]
-            omega_nlm += 1
+            if ((n-l)%2) == 0: # (n-l) even
+                
+                # sum over m for each combination of n and l
+                for m in range(-l,l+1): # m goes from -l to l
+                    moment_sum[zd_index] += sq_moments[omega_index]
+                    omega_index += 1
+                zd_index += 1
     
-    
-    
-#%%
-
-    
-    
-    
-    
-    
-    
-    
+    # eqn. 10
+    zernike_descriptor = moment_sum**.5
+    return zernike_descriptor
--- Scripts/get_patch_coords.py
+++ Scripts/get_patch_coords.py
@@ -15,7 +15,7 @@ def read_ply(file_name,n_edges=3):
     ----------
     file_name : file.ply
     
-        Protein Connolly surface output file from EDTsurf in .ply format
+        Protein Connolly surface file in .ply format (output file from EDTsurf)
         
     n_edges : int, optional
         
@@ -26,24 +26,28 @@ def read_ply(file_name,n_edges=3):
     -------
     vertices : numpy array of float
     
-        Points on the Connolly surface (x,y,z) and color code (r,g,b) from EDTsurf
-        Recommended color based on nearest atom
+        Points on the Connolly surface (x,y,z) and color code (r,g,b) from EDTsurf.
+        Recommended color based on nearest atom.
     
     faces : numpy array of float
         
-        Information used by ply files to create a triangular mesh
+        Information used by ply files to create a triangular mesh.
         [n_edges=3,point_index_1:n_edges,r,g,b]
     
     """
     
     import numpy as np
-           
+    
+    # read file
     with open(f"{file_name}") as f:
         text = f.read()
-    text = text.split('end_header') # remove header
+        
+    # remove header, spaces, new line characters
+    text = text.split('end_header') 
     text = text[1].strip()
     text = text.split('\n') 
     
+    # save vertex and face information
     vertices = []
     faces = []
     for i in range(len(text)):
@@ -59,7 +63,7 @@ def read_ply(file_name,n_edges=3):
 
 #%%
 
-def make_surface_patches(surface_points,n_patches=1,patch_radius=8,atom_centers=None):
+def make_surface_patches(surface_points,n_patches=1,patch_radius=8,features=True,atom_centers=None):
     
     """
     
@@ -67,7 +71,7 @@ def make_surface_patches(surface_points,n_patches=1,patch_radius=8,atom_centers=
     ----------
     surface_points : numpy array of float
         
-        Coordinates (and opt. color code) of protein surface points.
+        Coordinates, color code, (and opt. custom features) of protein surface points.
         Recommended to use vertices output from read_ply().
         
     n_patches : int, optional
@@ -77,12 +81,18 @@ def make_surface_patches(surface_points,n_patches=1,patch_radius=8,atom_centers=
     patch_radius : float, optional
     
         The radius of the generated protein patch in Angstroms. The default is 8.
+        
+    features: bool, optional
+    
+        Include pre-defined features along with surface coordinates.
+        The default is True.
     
     atom_centers : file.pdb, optional
     
         The protein file from which to include atom center information.
         This may be desired to distinguish a patch surface from its inverse.
-        The default is None.
+        Using this with features = True may cause issues downstream unless features
+        for atom center coordinates are added manually. The default is None.
 
     Returns
     -------
@@ -91,6 +101,8 @@ def make_surface_patches(surface_points,n_patches=1,patch_radius=8,atom_centers=
         The dictionary contains the coordinates of all surface points (and opt
         atom centers) within patch_radius of a random surface point.
         Dictionary entries are named by the centerpoint_index of each patch.
+        If features = True, each dictionary entry will contain an array of coordinates
+        and an array of features.
 
     """
     
@@ -98,13 +110,17 @@ def make_surface_patches(surface_points,n_patches=1,patch_radius=8,atom_centers=
     from numpy.random import default_rng
 
     rng = default_rng(42) # seed=42 for reproducibility
+    # choose random patch centers (no duplicates)
     patch_center = rng.choice(len(surface_points), size=n_patches, replace=False)
     
     patch_dict = {}
     
     for centerpoint_index in patch_center:
-
+        
+        # initialize patch with centerpoint
         patch = [surface_points[centerpoint_index][:3]]
+        if features == True:
+            feat = [surface_points[centerpoint_index][3:]]
         
         # calculate which points are within the patch radius
         for i in surface_points: 
@@ -113,27 +129,35 @@ def make_surface_patches(surface_points,n_patches=1,patch_radius=8,atom_centers=
             r = ((surface_points[centerpoint_index][0] - i[0])**2 + 
                  (surface_points[centerpoint_index][1] - i[1])**2 + 
                  (surface_points[centerpoint_index][2] - i[2])**2)**.5
+            
+            # add point coordinates to patch
             if r <= patch_radius:
                 patch.append(i[:3])
+                
+                # (opt. - add features to patch)
+                if features == True:
+                    feat.append(i[3:])
         
         # calculate which atom centers are within the patch radius
-        if atom_centers != None:
+        if atom_centers is not None:
             atoms = get_atom_centers(f"{atom_centers}")
             for i in atoms: 
                 r = ((surface_points[centerpoint_index][0] - i[0])**2 + 
                      (surface_points[centerpoint_index][1] - i[1])**2 + 
                      (surface_points[centerpoint_index][2] - i[2])**2)**.5
+                
+                # add atom center coordinates to patch
                 if r <= patch_radius:
                     patch.append(i)
         
         patch = np.asarray(patch)
+        feat = np.asarray(feat)
         
-        patch_dict[centerpoint_index] = patch
+        patch_dict[centerpoint_index] = patch, feat
         
     return patch_dict
 
-#%% including atom coordinates
-
+#%%
 def get_atom_centers(file_name):
     
     """
@@ -161,6 +185,7 @@ def get_atom_centers(file_name):
     structure = parser.get_structure(f"{file_name}", f"{file_name}")  
     atoms = structure.get_atoms()
     
+    # obtain all atom coordinates from structure
     atom_centers = []
     for atom in atoms:
         if 'W' not in atom.full_id[3][0]: # remove waters
@@ -172,7 +197,7 @@ def get_atom_centers(file_name):
 
 #%%
 
-def scale_patch(patch_dict,patch_radius=8):
+def scale_patch(patch_dict,patch_radius=8,features=True):
     
     """
 
@@ -187,6 +212,11 @@ def scale_patch(patch_dict,patch_radius=8):
     patch_radius : float, optional
     
         The radius of the generated protein patch in Angstroms. The default is 8.
+        
+    features: bool, optional
+    
+        Indicate whether patch_dict was created with pre-defined features along
+        with surface coordinates. The default is True.
 
     Returns
     -------
@@ -204,11 +234,24 @@ def scale_patch(patch_dict,patch_radius=8):
     scaled_dict = {}
     
     for patch in patch_dict.keys():
+        
+        # adjust indexing if features are included in dictionary
+        if features == True:
+            coordinates = patch_dict[patch][0]
+        else:
+            coordinates = patch_dict[patch]
+        
+        # scale patch coordinates by radius
         scaled_patch = []
-        for point in patch_dict[patch]:
-            scaled_point = (point - patch_dict[patch][0])/patch_radius
+        for point in coordinates:
+            scaled_point = (point - coordinates[0])/patch_radius
             scaled_patch.append(scaled_point)
         scaled_patch = np.asarray(scaled_patch)
-        scaled_dict[patch] = scaled_patch
+        
+        # adjust indexing if features are included in dictionary
+        if features == True:
+            scaled_dict[patch] = scaled_patch, patch_dict[patch][1]
+        else:
+            scaled_dict[patch] = scaled_patch
 
     return scaled_dict
